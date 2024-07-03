0924347d03963bf953c91572d29677aac495702b
Your Name <you@example.com>
2024-07-03 23:06:13 +0300

    proje 1.adım tamamlandı...

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,8 +1,11 @@
 import { useRef } from "react";
 
 import Input from "./Input.jsx";
+import Modal from './Modal.jsx';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -17,7 +20,8 @@ export default function NewProject({onAdd}) {
       enteredDescription.trim() === '' || 
       enteredDueDate.trim() === ''
     ) {
-      // show the error modal 
+      modal.current.open();
+      return;
     }
 
     onAdd({
@@ -28,17 +32,30 @@ export default function NewProject({onAdd}) {
   }  
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Okay">
+      <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+      <p className='text-stone-600 mb-4'>
+        Oops ... looks lie you forgot to enter a value.
+      </p>
+      <p className='text-stone-600 mb-4'>
+        Please make sure you provide a valid value for every input field.
+      </p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">
+          <button 
+            className="text-stone-800 hover:text-stone-950" 
+            onClick={onCancel}
+          >
             Cancel
           </button>
         </li>
         <li>
           <button 
             className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            Onclick={handleSave}
+            onClick={handleSave}
           >
             Save
           </button>
@@ -49,6 +66,7 @@ export default function NewProject({onAdd}) {
         <Input ref={description} label="Description" textarea />
         <Input type="date" ref={dueDate} label="Due Date"/>
       </div>
-    </div>   
+    </div>
+    </>  
   );  
 }
